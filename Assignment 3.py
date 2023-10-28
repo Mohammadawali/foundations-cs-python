5b1e1dc4bcb35830a7b62813e322b7c5a5ae2507
Mohammadawali <148576713+Mohammadawali@users.noreply.github.com>
2023-10-28 22:58:01 +0300

    main() function

--- Assignment 3.py
+++ Assignment 3.py
@@ -5,3 +5,4 @@ def main():
 
 # Call the main function
 main()
+
--- Untitled-1.py
+++
@@ -1,7 +0,0 @@
-def main():
-    # Prompt for the user's name
-    name = input("Enter your name: ")
-    print(f"Welcome, {name}!")
-
-# Call the main function
-main()
