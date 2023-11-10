60ac54e8f7994ac38735dbda2cda2902defe82ee
Mohammadawali <148576713+Mohammadawali@users.noreply.github.com>
2023-11-10 23:28:55 +0200

    Add main

--- Midterm Project/Midterm Project.py
+++ Midterm Project/Midterm Project.py
@@ -17,17 +17,49 @@ def importTabs():
 def exit():
     pass
 def displayMenu():
-  print("Welcome to FCS Browser Tabs Simulation")
-  print("1. Open Tab")
-  print("2. Close Tab")
-  print("3. Switch Tab")
-  print("4. Display All Tabs")
-  print("5. Open Nested Tabs")
-  print("6. Clear All Tabs")
-  print("7. Save Tabs")
-  print("8. Import Tabs")
-  print("9. Exit")
-  print("-----------------------------------")
-  
-  
-         
+    
+    print("Welcome to FCS Browser Tabs Simulation")
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tabs")
+    print("6. Clear All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
+    print("-----------------------------------")
+    
+def main():
+    while True:
+        displayMenu()
+        choice = input("Enter your choice")
+        if choice == '1':
+            openTab()
+        elif choice == '2':
+            closeTab()
+        elif choice == '3':
+            switchTab()
+        elif choice == '4':
+            displayAllTabs()
+        elif choice == '5':
+            openNastedTab()
+        elif choice == '6':
+            clearAllTabs()
+        elif choice == '7':
+            saveTabs()
+        elif choice == '8':
+            importTabs()
+        elif choice == '9':
+            exit()
+            break
+        else:
+            print("invalid choice. Please choose a number from Menu. ")
+            
+if __name__ == "__main__":
+    main()
+            
+            
+                      
+            
+                      
