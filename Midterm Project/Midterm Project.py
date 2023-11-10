44dae665c6875197f5d506ed8a7695283517c746
Mohammadawali <148576713+Mohammadawali@users.noreply.github.com>
2023-11-10 20:53:32 +0200

    Add Display Menu

--- Midterm Project/Midterm Project.py
+++ Midterm Project/Midterm Project.py
@@ -14,3 +14,15 @@ def importTabs():
     pass
 def exit():
     pass
+def displayMenu():
+  print("Welcome to FCS Browser Tabs Simulation")
+  print("1. Open Tab")
+  print("2. Close Tab")
+  print("3. Switch Tab")
+  print("4. Display All Tabs")
+  print("5. Open Nested Tabs")
+  print("6. Clear All Tabs")
+  print("7. Save Tabs")
+  print("8. Import Tabs")
+  print("9. Exit")
+  print("-----------------------------------")    
